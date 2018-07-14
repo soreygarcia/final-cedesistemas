31f315b167bee5d821b451f0cb44a51e11b8112c
Sorey García <soreygarcia@gmail.com>
2018-07-14 09:44:11 -0500

    Se crea archivo de recursos

--- Traveler/Traveler/Services/Api/ApiService.cs
+++ Traveler/Traveler/Services/Api/ApiService.cs
@@ -1,21 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Traveler.Models;
+using Traveler.Services.Network;
 
 namespace Traveler.Services.Api
 {
     public class ApiService : IApiService
     {
-        public Task<ContentResponse<PlaceModel>> CreatePlace()
+        private readonly INetworkService _networkService;
+
+        public ApiService(INetworkService networkService)
         {
-            throw new NotImplementedException();
+            _networkService = networkService;
         }
 
-        public Task<ContentResponse<List<PlaceModel>>> GetAllPlaces()
+        public async Task<ContentResponse<PlaceModel>> CreatePlace()
         {
-            throw new NotImplementedException();
+            if (_networkService.IsConnected())
+            {
+                using (HttpClient client = new HttpClient())
+                {
+
+                }
+            }
+            else
+            {
+                return new ContentResponse<PlaceModel>()
+                {
+                    HttpResponse = new HttpResponseMessage(HttpStatusCode.RequestTimeout)
+                };
+            }
+        }
+
+        public async Task<ContentResponse<List<PlaceModel>>> GetAllPlaces()
+        {
+            if (_networkService.IsConnected())
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var result = await client.GetAsync("");
+                }
+            }
+            else
+            {
+                return new ContentResponse<List<PlaceModel>>()
+                {
+                    HttpResponse = new HttpResponseMessage(HttpStatusCode.RequestTimeout)
+                };
+            }
         }
     }
 }
