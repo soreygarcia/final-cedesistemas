de932abae344075d20e405a64e4b102f1159f0d9
Sorey García <soreygarcia@gmail.com>
2018-07-14 11:30:06 -0500

    Navegar a Waze

--- Traveler/Traveler/ViewModels/PlaceDetailPageViewModel.cs
+++ Traveler/Traveler/ViewModels/PlaceDetailPageViewModel.cs
@@ -4,7 +4,9 @@ using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
 using Traveler.Models;
+using Xamarin.Forms;
 
 namespace Traveler.ViewModels
 {
@@ -12,9 +14,27 @@ namespace Traveler.ViewModels
 	{
         public PlaceModel SelectedPlace { get; set; }
 
+        public ICommand OpenWazeCommand { get; set; }
+
         public PlaceDetailPageViewModel(INavigationService navigationService) : base(navigationService)
         {
-            
+            OpenWazeCommand = new DelegateCommand(OpenWaze);
+        }
+
+        private void OpenWaze()
+        {
+            try
+            {
+                //TODO: Esto deberia ir en un servicio
+                Device.OpenUri(new Uri($"https://waze.com/ul?ll={SelectedPlace.Latitude},{SelectedPlace.Longitude}&navigate=yes"));
+            }
+            catch
+            {
+                //if (Device.RuntimePlatform.Equals(Device.iOS))
+
+                if (Device.RuntimePlatform.Equals(Device.Android))
+                        Device.OpenUri(new Uri($"https://play.google.com/store/apps/details?id=com.waze"));
+            }
         }
 
         public override void OnNavigatingTo(NavigationParameters parameters)
