c5ae13d498e461c0eb744ace988c58ce2ebb0da8
Sorey García <soreygarcia@gmail.com>
2018-07-14 09:51:49 -0500

    Api service finalizado

--- Traveler/Traveler/Services/Api/ApiService.cs
+++ Traveler/Traveler/Services/Api/ApiService.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Traveler.Models;
+using Traveler.Resources;
 using Traveler.Services.Network;
 
 namespace Traveler.Services.Api
@@ -18,13 +20,24 @@ namespace Traveler.Services.Api
             _networkService = networkService;
         }
 
-        public async Task<ContentResponse<PlaceModel>> CreatePlace()
+        public async Task<ContentResponse<PlaceModel>> CreatePlace(PlaceModel placeModel)
         {
             if (_networkService.IsConnected())
             {
                 using (HttpClient client = new HttpClient())
                 {
+                    var url = AppConfigurations.ApiRoot + AppConfigurations.GetAllPlacesEndpoint;
 
+                    var body = new StringContent(JsonConvert.SerializeObject(placeModel), Encoding.UTF8, "application/json");
+                    var result = await client.PostAsync(url, body);
+
+                    string data = await result.Content.ReadAsStringAsync();
+
+                    return new ContentResponse<PlaceModel>()
+                    {
+                        HttpResponse = result,
+                        Data = JsonConvert.DeserializeObject<PlaceModel>(data)
+                    };
                 }
             }
             else
@@ -42,7 +55,15 @@ namespace Traveler.Services.Api
             {
                 using (HttpClient client = new HttpClient())
                 {
-                    var result = await client.GetAsync("");
+                    var url = AppConfigurations.ApiRoot + AppConfigurations.GetAllPlacesEndpoint;
+                    var result = await client.GetAsync(url);
+                    string data = await result.Content.ReadAsStringAsync();
+
+                    return new ContentResponse<List<PlaceModel>>()
+                    {
+                        HttpResponse = result,
+                        Data = JsonConvert.DeserializeObject<List<PlaceModel>>(data)
+                    };
                 }
             }
             else
