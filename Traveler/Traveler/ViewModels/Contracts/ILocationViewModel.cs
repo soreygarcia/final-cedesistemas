76ab806fe95c55b2c51e943c53016f1046620a74
Sorey García <soreygarcia@gmail.com>
2018-07-14 11:42:37 -0500

    Behaviours compilando

---
+++ Traveler/Traveler/ViewModels/Contracts/ILocationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Traveler.ViewModels.Contracts
+{
+    public interface ILocationViewModel
+    {
+        string Title { get; set; }
+        string Description { get; set; }
+        double Latitude { get; set; }
+        double Longitude { get; set; }
+        ICommand Command { get; set; }
+    }
+}
